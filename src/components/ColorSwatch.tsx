36bae1f2544fd2deea2f5ae3477b18724d004682
Artem Sapegin <artem@sapegin.me>
2025-10-31 11:46:22 +0100

    Update deps

--- src/components/ColorSwatch.tsx
+++ src/components/ColorSwatch.tsx
@@ -1,10 +1,5 @@
 import { Box } from './Box';
 
 export function ColorSwatch({ color }: { color: string }) {
-  return (
-    <Box
-      height="6rem"
-      style={{ backgroundColor: color }}
-    />
-  );
+  return <Box height="6rem" style={{ backgroundColor: color }} />;
 }
