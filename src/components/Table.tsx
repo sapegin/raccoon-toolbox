6830f6b5c0308e8aa772895d43e5dfaace24e769
Artem Sapegin <artem@sapegin.me>
2025-11-01 12:04:39 +0100

    Update ESLint

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -5,39 +5,39 @@ export function Table({ children }: { children: ReactNode }) {
   return (
     <table
       className={css({
-      width: '100%',
-      borderCollapse: 'collapse',
-      '& th': {
-        textAlign: 'left',
-        paddingBlock: 's',
-        paddingInline: 'm',
-        borderBottom: '1px solid',
-        borderColor: 'lightBorder',
-        color: 'textForeground',
-        fontSize: 's',
-        fontWeight: 'normal',
-      },
-      '& th:first-child': {
-        paddingLeft: 0,
-      },
-      '& th:last-child': {
-        paddingRight: 0,
-      },
-      '& td': {
-        paddingBlock: 's',
-        paddingInline: 'm',
-        borderBottom: '1px solid',
-        borderColor: 'lightBorder',
-      },
-      '& td:first-child': {
-        paddingLeft: 0,
-      },
-      '& td:last-child': {
-        paddingRight: 0,
-      },
-      '& tbody tr:hover': {
-        backgroundColor: 'lineHighlightBackground',
-      },
+        width: '100%',
+        borderCollapse: 'collapse',
+        '& th': {
+          textAlign: 'left',
+          paddingBlock: 's',
+          paddingInline: 'm',
+          borderBottom: '1px solid',
+          borderColor: 'lightBorder',
+          color: 'textForeground',
+          fontSize: 's',
+          fontWeight: 'normal',
+        },
+        '& th:first-child': {
+          paddingLeft: 0,
+        },
+        '& th:last-child': {
+          paddingRight: 0,
+        },
+        '& td': {
+          paddingBlock: 's',
+          paddingInline: 'm',
+          borderBottom: '1px solid',
+          borderColor: 'lightBorder',
+        },
+        '& td:first-child': {
+          paddingLeft: 0,
+        },
+        '& td:last-child': {
+          paddingRight: 0,
+        },
+        '& tbody tr:hover': {
+          backgroundColor: 'lineHighlightBackground',
+        },
       })}
     >
       {children}
