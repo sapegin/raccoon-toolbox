0634cfb6f5c1e80e9084d0d0df5805230584f1d6
Artem Sapegin <artem@sapegin.me>
2025-11-15 11:41:45 +0100

    Tweak whitespace

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -1,5 +1,12 @@
 import { Grid, type GridProps } from '../../styled-system/jsx';
 
 export function Screen(props: GridProps) {
-  return <Grid gap="m" {...props} height="100%" cursor="default" />;
+  return (
+    <Grid
+      gap={{ base: 0, desktop: 's' }}
+      {...props}
+      height="100%"
+      cursor="default"
+    />
+  );
 }
