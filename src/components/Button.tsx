1bbdf2b2e56b541d99dd3a82fbbf6d85f0cd53ae
Artem Sapegin <artem@sapegin.me>
2026-04-24 09:55:35 +0200

    Clean up button styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -25,7 +25,7 @@ export function Button({
     <button
       {...props}
       className={clsx(
-        'button focus-outline',
+        'button',
         variantClasses[variant],
         sizeClasses[size],
         className
